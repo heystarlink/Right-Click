91223bc7cb67c69cd70af3ab18e02a0632537b17
kenny <r1dpanduh@gmail.com>
2023-10-23 22:53:25 +0800

    Update contentscript.js

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-  let url = window.location.href
-  if(let.indexOf("zsxq") > 1){
+  var url = window.location.href
+  if(url.indexOf("zsxq") > 1){
     var divs = document.getElementsByClassName("js-disable-copy");
     for(var i = 0; i < divs.length; i++){
         divs[i].classList.remove("js-disable-copy");
